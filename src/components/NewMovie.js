e4b17e8362c133e38ad5f4127ab2c3bdf486a007
b3ngineer <ben.harris.when@gmail.com>
2018-10-16 11:05:26 -0600

    Contains rudimentary validation

--- src/components/NewMovie.js
+++ src/components/NewMovie.js
@@ -4,6 +4,11 @@ import MovieCategory from "./MovieCategory";
 import { connect } from "react-redux";
 import "./css/Movies.css";
 import { movies } from "../store/actions";
+import "react-notifications/lib/notifications.css";
+import {
+  NotificationContainer,
+  NotificationManager
+} from "react-notifications";
 
 const { addMovie } = movies;
 
@@ -15,15 +20,47 @@ class NewMovie extends Component {
   };
 
   handleSaveClick = () => {
-    // TODO: handle missing this.newCategory
-    if (this.newName) {
-      this.props.addMovie(this.newName, this.newCategory);
-      this.clear();
+    if (!this.newName) {
+      return NotificationManager.warning(
+        "Please fill in the movie name to continue",
+        "Oops! You can't save yet...",
+        5000
+      );
     }
+
+    if (!this.validName()) {
+      return NotificationManager.error(
+        "The movie name contains invalid characters",
+        "Oops! You can't save yet...",
+        5000
+      );
+    }
+
+    if (!this.newCategory) {
+      return NotificationManager.warning(
+        "Please select a category to continue",
+        "Oops! You can't save yet...",
+        5000
+      );
+    }
+
+    this.props.addMovie(this.newName, this.newCategory);
+    this.clear();
+  };
+
+  validName = () => {
+    return /^[a-z0-9\s]*$/i.test(this.newName);
   };
 
   handleNameChange = e => {
     this.newName = e.currentTarget.value;
+    if (!this.validName()) {
+      NotificationManager.error(
+        "Invalid character",
+        "Only letters and numbers are allowed",
+        5000
+      );
+    }
   };
 
   handleCategoryChange = e => {
@@ -33,6 +70,7 @@ class NewMovie extends Component {
   render() {
     return (
       <React.Fragment>
+        <NotificationContainer />
         <Row>
           <Col xs={1} sm={2} md={2}>
             <div className="space-bottom" />
